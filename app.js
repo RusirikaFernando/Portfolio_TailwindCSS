4e9cab190d2aadd48b619638cd4d7fdfc46e08bd
RusirikaFernando <thathsaranif7@gmail.com>
2024-06-04 22:30:01 +0530

    added dark mode using tailwind CSS

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const hamburger = document.querySelector("#hamburger")
 const menu = document.querySelector("#menu")
+const body = document.querySelector("body")
+const moon = document.querySelector("#moon")
+const hLink = document.querySelectorAll("#hLink")
 
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("hidden")
+    
+})
+
+hLink.forEach(hLink =>{
+    hLink.addEventListener("click", ()=>{
+        menu.classList.toggle("hidden")
+        // hamburger.classList.toggle("bg-white")
+    })
+})
+
+moon.addEventListener("click", () => {
+    body.classList.toggle("dark")
     
 })
